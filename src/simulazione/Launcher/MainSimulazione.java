528f42654b4545e27afcb4b023cc8c5b816b3f0a
david <david@PC-7458>
2019-09-05 00:01:55 +0200

    Aggiunta pagina iniziale controllare ultime cose

--- src/simulazione/Launcher/MainSimulazione.java
+++ src/simulazione/Launcher/MainSimulazione.java
@@ -1,10 +1,18 @@
 package simulazione.Launcher;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
+import javax.imageio.ImageIO;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -13,8 +21,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileFilter;
-import org.graphstream.ui.swingViewer.ViewPanel;
-import org.graphstream.ui.view.Viewer;
 import network.CityGraph;
 import network.CityGraphXMLParser;
 import simulazione.scenario.Regole;
@@ -23,16 +29,46 @@ import util.Param;
 import veicoli.Macchina;
 import veicoli.Vehicle;
 
-public class MainSimulazione extends JFrame {
+public class MainSimulazione {
+
 	private static final long serialVersionUID = 1L;
 	private CityGraph grafo;
 	private double numeroVeicoli = 0.0;
 	private double periodoGenerazione = 0.0;
 	private Scenario scenario;
 	private String nome;
+	private JFrame frame = new JFrame("Simulatore");
+	private boolean temp = true;
 
 	public MainSimulazione() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private class MainPanel extends JPanel {
+
+		private BufferedImage sfondo = null;
+
+		public MainPanel() {
+			try {
+				sfondo = ImageIO.read(getClass().getResource("/sfondoMain.png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			setSize(1388, 786);
+			setLayout(null);
+			MouseListener listener = new MouseAdapter() {
+
+				public void mouseClicked(MouseEvent e) {
+					temp = false;
+					frame.dispose();
+				}
+			};
+			addMouseListener(listener);
+		}
+
+		public void paintComponent(Graphics g) {
+			g.drawImage(sfondo, 0, 0, null);
+		}
 	}
 
 	// GETTER ///////////////////////////////
@@ -55,12 +91,9 @@ public class MainSimulazione extends JFrame {
 
 	// METHODS ///////////////////////////////
 
-	private void start(String directoryXMLGraph) throws InterruptedException {
+	public void start(String directoryXMLGraph) throws InterruptedException {
 		this.grafo = (CityGraph) CityGraphXMLParser.getGraph("Cosenza", directoryXMLGraph);
 		grafo.display().disableAutoLayout();
-		// this.numeroVeicoli = numeroVeicoli;
-		// this.periodoGenerazione = periodoDiGenerazione;
-		// this.scenario = scenario;
 		this.nome = "seed[" + Param.seed + "]";
 		// Param.resetRandom();
 		Param.setSeed(389);
@@ -77,7 +110,23 @@ public class MainSimulazione extends JFrame {
 		grafo.startSimulation();
 	}
 
-	private File chooseFile() {
+	public void sfondo() {
+		JPanel p = new MainPanel();
+
+		frame.setSize(1220, 740);
+		int x = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
+		int y = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
+		frame.setBounds(x - (1220 / 2), y - (740 / 2), 1220, 740);
+		frame.add(p);
+		frame.setVisible(true);
+		while (temp) {
+			frame.revalidate();
+			frame.repaint();
+		}
+
+	}
+
+	public File chooseFile() {
 		JFileChooser fc = new JFileChooser();
 
 		fc.setFileFilter(new FileFilter() {
@@ -96,7 +145,7 @@ public class MainSimulazione extends JFrame {
 		});
 
 		fc.setCurrentDirectory(new File(System.getProperty("user.dir") + File.separator + "XMLConfig"));
-		int n = fc.showOpenDialog(this);
+		int n = fc.showOpenDialog(frame);
 
 		if (n == JFileChooser.APPROVE_OPTION) return fc.getSelectedFile();
 		else System.exit(-1);
@@ -104,25 +153,34 @@ public class MainSimulazione extends JFrame {
 
 	}
 
-	private void inputParam() {
-		JPanel p = new JPanel(new GridLayout(4, 2, 5,10));
+	public void inputParam() {
+		JPanel p = new JPanel(new GridLayout(4, 2, 5, 10));
+		
 		JTextField veicoli = new JTextField(10);
 		JTextField perGenerazione = new JTextField(10);
-
 		JComboBox scenarioScelto = new JComboBox(Scenario.values());
-		String [] semafori = {"SEMAFORO CLASSICO","SEMAFORO A SOGLIA"}; 
+		String[] semafori = { "SEMAFORO CLASSICO", "SEMAFORO A SOGLIA" };
 		JComboBox semaforoScelto = new JComboBox(semafori);
-
-		p.add(new JLabel("Numero di veicoli: "));
+		
+		JLabel veic = new JLabel("Inserire numero di veicoli: ");
+		veic.setFont( new Font(Font.SANS_SERIF, Font.CENTER_BASELINE, 15));
+		p.add(veic);
 		p.add(veicoli);
-		p.add(new JLabel("tempo generazione veicoli (in millisecondi): "));
+		
+		JLabel tempo = new JLabel("Inserire tempo generazione veicoli (in millis): ");
+		tempo.setFont( new Font(Font.SANS_SERIF, Font.CENTER_BASELINE, 15));
+		p.add(tempo);
 		p.add(perGenerazione);
-		p.add(new JLabel("Scegli scenario: "));
+		
+		JLabel scen = new JLabel("Scegliere scenario: ");
+		scen.setFont( new Font(Font.SANS_SERIF, Font.CENTER_BASELINE, 15));
+		p.add(scen);
 		p.add(scenarioScelto);
-		scenarioScelto.setBackground(new Color(255,255,255));
-		p.add(new JLabel("Scegli tipo di semaforo: "));
+		
+		JLabel sem =new JLabel("Scegliere il tipo di semaforo: ");
+		sem.setFont( new Font(Font.SANS_SERIF, Font.CENTER_BASELINE, 15));
+		p.add(sem);
 		p.add(semaforoScelto);
-		semaforoScelto.setBackground(new Color(255,255,255));
 
 		do {
 			int res = JOptionPane.showConfirmDialog(null, p, "PARAMETRI DI INPUT: ", JOptionPane.OK_CANCEL_OPTION);
@@ -133,14 +191,13 @@ public class MainSimulazione extends JFrame {
 
 				if (perGenerazione.getText().matches("[0-9]*") && !perGenerazione.getText().trim().equals(""))
 					periodoGenerazione = Double.parseDouble(perGenerazione.getText());
-				else periodoGenerazione = 0;
-				
-				if(semaforoScelto.getSelectedItem().equals("SEMAFORO CLASSICO")) Param.setSemaforo(true);
+				else periodoGenerazione = 1;
+
+				if (semaforoScelto.getSelectedItem().equals("SEMAFORO CLASSICO")) Param.setSemaforo(true);
 				else Param.setSemaforo(false);
-				
+
 				scenario = (Scenario) scenarioScelto.getSelectedItem();
-				
-				
+
 			} else System.exit(-1);
 			String errore = "";
 			if (numeroVeicoli == 0 || periodoGenerazione == 0) {
@@ -151,9 +208,9 @@ public class MainSimulazione extends JFrame {
 		} while (numeroVeicoli == 0 || periodoGenerazione == 0);
 	}
 
-
 	public static void main(String[] args) throws IOException {
 		MainSimulazione sim = new MainSimulazione();
+		sim.sfondo();
 		String directory = sim.chooseFile().toString();
 		sim.inputParam();
 		try {
